aae1fb70c724652e07940e3792afe99889cf0d52
Ilya Shiyan <ilya.shiyan97@mail.ru>
2022-12-09 07:45:37 +0300

    1.1

---
+++ src/main/java/Main.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Student student1 = new Student("Ivan", 101, 1, 4);
+        Student student2 = new Student("Liza", 101, 2, 5);
+        Student student3 = new Student("Ilya", 101, 3, 3);
+        Student student4 = new Student("Anton", 102, 1, 2);
+        Student student5 = new Student("Poul", 102, 1, 1);
+        Student student6 = new Student("John", 101, 1, 4);
+        Student student7 = new Student("Masha", 102, 2, 2);
+
+        List<Student> students = new ArrayList<>(Arrays.asList(student1, student2, student3, student4, student5, student6, student7));
+
+        Iterator<Student> iterator = students.listIterator();
+        while (iterator.hasNext()) {
+            Student i = iterator.next();
+            if (i.getGrades() < 3) {
+                iterator.remove();
+            } else i.setCourse(i.getCourse()+1);
+
+        }
+        for (Student i : students) {
+            System.out.println(i);
+
+        }
+
+    }
+}
---
+++ src/main/java/Student.java
@@ -0,0 +1,49 @@
+public class Student {
+    private String name;
+    private int group;
+    private int course;
+    private int grades;
+
+    public Student(String name, int group, int course, int grades) {
+        this.name = name;
+        this.group = group;
+        this.course = course;
+        this.grades = grades;
+    }
+
+    @Override
+    public String toString(){return String.valueOf(course);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getGroup() {
+        return group;
+    }
+
+    public int getCourse() {
+        return course;
+    }
+
+    public int getGrades() {
+        return grades;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setGroup(int group) {
+        this.group = group;
+    }
+
+    public void setCourse(int course) {
+        this.course = course;
+    }
+
+    public void setGrades(int grades) {
+        this.grades = grades;
+    }
+}
